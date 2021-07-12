72f5ffea279dccb7d55fbbabd0c8dfc3153fa27a
Baptiste DELPHIN <baptiste.delphin@gmail.com>
2021-07-12 18:59:21 +0200

    fixed arduino sketch

--- CAN_BUS_C4/CAN_BUS_C4.ino
+++ CAN_BUS_C4/CAN_BUS_C4.ino
@@ -31,12 +31,13 @@ void setup() {
     while (CAN.begin(canSpeed, MCP_8MHz) != CAN_OK) {
       delay(100);
     }
+    Serial.println("CAN OK !");
 }
 
 void loop() 
 {
-    unsigned char len = 0;
-    byte buf[8];
+    //unsigned char len = 0;
+    //byte buf[8];
     
     //  - timeSinceReverseDisengaged: this timer waits a few seconds after disengaging reverse
     //    before switching back to OpenAuto, in case we're parallel parking and switching back and
@@ -61,14 +62,17 @@ void loop()
         CAN.readMsgBuf(&len, buffer);
         int id = CAN.getCanId();
 
+        //can_debug();
+
         // reverse gear state
         if (id == 246 && len == 8) 
         {
-            if (buf[7] & 0b00100000) 
+            //Serial.println(buffer[7]);
+            if (buffer[7] == 160) 
             {
                 // Reverse gear engaged
-
-                Serial.println("REVERSE_ENGAGED");
+                if(!reverseEngaged)
+                  Serial.println("REVERSE_ENGAGED");
                 
                 reverseEngaged = true;
                 shouldStopReverse = false;
@@ -84,59 +88,132 @@ void loop()
                 }
             }   
         }
-        else if (id == 543) 
+        else
+        if (id == 543) 
         {
-            if (buf[0] & 0b00000000) 
-            {
-                // no button pressed
-                if(mute_pressed)
-                {
-                    mute_pressed = false;
-                }
-                if(next_pressed)
-                {
-                    next_pressed = false;
-                }
-            }
-            else if (buf[0] & 0b00001000) 
-            {
-                // vol+ button pressed
-                Serial.println("VOL_UP");
-            }
-            else if (buf[0] & 0b00000100) 
-            {
-                // vol- button pressed
-                Serial.println("VOL_DOWN");
-            }
-            else if (buf[0] & 0b00001100) 
-            {
-                // MUTE button pressed
-                if(!mute_pressed)
-                {
-                    mute_pressed = true;
-                    if (mute)
-                    {
-                        mute = false;
-                        Serial.println("PLAY");
-                    }
-                    else
-                    {
-                        mute = true;
-                        Serial.println("PAUSE");
-                    }
-                } 
-            }
-            else if (buf[0] & 0b10000000) 
-            {
-                // NEXT button pressed
-                if(!next_pressed)
-                {
-                    next_pressed = true;
-                    Serial.println("NEXT");
-                }
-            }
+          if (buffer[0] == 0) 
+          {
+              // no button pressed
+              if(mute_pressed)
+              {
+                  mute_pressed = false;
+              }
+              if(next_pressed)
+              {
+                  next_pressed = false;
+              }
+          }
+          else if (buffer[0] == 8) 
+          {
+              // vol+ button pressed
+              Serial.println("VOL_UP");
+          }
+          else if (buffer[0] == 4) 
+          {
+              // vol- button pressed
+              Serial.println("VOL_DOWN");
+          }
+          else if (buffer[0] == 12) 
+          {
+              // MUTE button pressed
+              //Serial.println("TOGGLE_PLAY");
+              if(!mute_pressed)
+              {
+                  mute_pressed = true;
+                  if (mute)
+                  {
+                      mute = false;
+                      Serial.println("PLAY");
+                  }
+                  else
+                  {
+                      mute = true;
+                      Serial.println("PAUSE");
+                  }
+              } 
+          }
+          else if (buffer[0] == 128) 
+          {
+              // NEXT button pressed
+              //Serial.println("NEXT");
+              if(!next_pressed)
+              {
+                  next_pressed = true;
+                  Serial.println("NEXT");
+              }
+          }
+//            if (buffer[0] & 0b00000000) 
+//            {
+//                // no button pressed
+//                if(mute_pressed)
+//                {
+//                    mute_pressed = false;
+//                }
+//                if(next_pressed)
+//                {
+//                    next_pressed = false;
+//                }
+//            }
+//            else if (buffer[0] & 0b00001000) 
+//            {
+//                // vol+ button pressed
+//                Serial.println("VOL_UP");
+//            }
+//            else if (buffer[0] & 0b00000100) 
+//            {
+//                // vol- button pressed
+//                Serial.println("VOL_DOWN");
+//            }
+//            else if (buffer[0] & 0b00001100) 
+//            {
+//                // MUTE button pressed
+//                Serial.println("TOGGLE_PLAY");
+//                /*if(!mute_pressed)
+//                {
+//                    mute_pressed = true;
+//                    if (mute)
+//                    {
+//                        mute = false;
+//                        Serial.println("PLAY");
+//                    }
+//                    else
+//                    {
+//                        mute = true;
+//                        Serial.println("PAUSE");
+//                    }
+//                }*/ 
+//            }
+//            else if (buffer[0] & 0b10000000) 
+//            {
+//                // NEXT button pressed
+//                Serial.println("NEXT");
+//                /*if(!next_pressed)
+//                {
+//                    next_pressed = true;
+//                    Serial.println("NEXT");
+//                }*/
+//            }
         }
 
 
     }
+}
+
+void can_debug()
+{
+  Serial.print("FRAME:ID=");
+        Serial.print(CAN.getCanId());
+        Serial.print(":LEN=");
+        Serial.print(len);
+        
+        char tmp[3];
+        for(int i = 0; i<len; i++) {
+            Serial.print(":");
+            
+            snprintf(tmp, 3, "%02X", buffer[i]);
+            
+            Serial.print(tmp);
+        }
+
+        Serial.println();
 }
