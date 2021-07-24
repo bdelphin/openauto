94f9e96f939dc50f68866fa93f57e37637cb1e58
Baptiste DELPHIN <baptiste.delphin@gmail.com>
2021-07-24 16:50:01 +0200

    updated reverse action

--- script.py
+++ script.py
@@ -3,7 +3,7 @@ import time
 import subprocess
 
 try:
-    ser = serial.Serial('/dev/ttyACM0', 115200, timeout=1)
+    ser = serial.Serial('/dev/ttyUSB0', 115200, timeout=1)
     ser.flush()
 except serial.SerialException:
     print('SERIAL_ERROR')
@@ -13,14 +13,18 @@ time.sleep(2)
 
 while(1):
     command = ser.readline().decode('utf-8').rstrip()
+    if command:
+        print(command)
+    else:
+        time.sleep(1)
     if(command == 'REVERSE_ENGAGED'):
         # switch to camera
-        print('reverse')
+        subprocess.call('echo 1 > /sys/class/gpio/gpio20/active_low', shell=True)
     elif(command == 'REVERSE_DISENGAGED'):
         # switch back to OpenAuto
-        print('normal')
+        subprocess.call('echo 0 > /sys/class/gpio/gpio20/active_low', shell=True)
     elif(command == 'VOL_UP'):
-        subprocess.call(["xdotool", "key", "F8"])    
+        subprocess.call(["xdotool", "key", "F8"])
     elif(command == 'VOL_DOWN'):
         subprocess.call(["xdotool", "key", "F7"])
     elif(command == 'PLAY'):
@@ -30,5 +34,5 @@ while(1):
     elif(command == 'NEXT'):
         subprocess.call(["xdotool", "key", "N"])
 
-    time.sleep(1)
+    #time.sleep(1)
 
